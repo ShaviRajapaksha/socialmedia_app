b11103c20ac18ab0215bd0262455014ebf77f12b
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-19 01:20:51 +0530

    feat: Refactor LoginScreen to accept a callback and add SignupScreen implementation

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  final VoidCallback show;
+  LoginScreen(this.show,{super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
---
+++ lib/screen/signup_screen.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SignupScreen extends StatefulWidget {
+  final VoidCallback show;
+  SignupScreen(this.show,{super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final email = TextEditingController();
+  FocusNode emailFocus = FocusNode();
+  final password = TextEditingController();
+  FocusNode passwordFocus = FocusNode();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            SizedBox(width: 96.w, height: 100.h),
+            Center(child: Image.asset('images/logo.png')),
+            SizedBox(height: 120.h),
+            Textfield(email, Icons.email, 'Email', emailFocus),
+            SizedBox(height: 15.h),
+            Textfield(password, Icons.lock, 'Password', passwordFocus),
+            SizedBox(height: 10.h),
+            Forgot(),
+            SizedBox(height: 10.h),
+            Login(),
+            SizedBox(height: 10.h),
+            Have(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget Have() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            "Don't have an account?",
+            style: TextStyle(fontSize: 14.sp, color: Colors.grey),
+          ),
+          Text(
+            "Signup",
+            style: TextStyle(
+              fontSize: 14.sp,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget Login() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: 44.h,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(10.r),
+        ),
+        child: Text(
+          'Login',
+          style: TextStyle(
+            fontSize: 24.sp,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget Forgot() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      child: Text(
+        'Forgot Password?',
+        style: TextStyle(
+          fontSize: 14.sp,
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget Textfield(
+    TextEditingController controller,
+    IconData icon,
+    String type,
+    FocusNode focusNode,
+  ) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: Container(
+        height: 44.h,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(5.r),
+        ),
+        child: TextField(
+          style: TextStyle(fontSize: 18.sp, color: Colors.black),
+          controller: controller,
+          focusNode: focusNode,
+          decoration: InputDecoration(
+            hintText: type,
+            prefixIcon: Icon(
+              icon,
+              color: focusNode.hasFocus ? Colors.black : Colors.grey,
+            ),
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 15.w,
+              vertical: 15.h,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5.r),
+              borderSide: BorderSide(color: Colors.grey, width: 2.w),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5.r),
+              borderSide: BorderSide(color: Colors.black, width: 2.w),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
